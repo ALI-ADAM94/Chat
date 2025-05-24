292283dc74d8a9c9b650bb276dc6f36b9b8644b7
ALI-ADAM94 <aliadam081194@gmail.com>
2025-05-24 11:18:37 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: process.env.VITE_LOGIN_PATH || "/Chat",
+    base: process.env.VITE_CHAT_APP_PATH || "/Chat",
 })
